bd45bbcdab0f76625f6ac31f4e6170d8d394a2a6
Dilshan Ariyawansha <it21173790@my.sliit.lk>
2025-04-20 15:15:29 +0530

    Initial commit - Product Catalog Microservice

--- index.js
+++ index.js
@@ -9,4 +9,4 @@ app.use(express.json());
 app.use('/products', require('./routes/productRoutes'));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port number ${PORT}`));
